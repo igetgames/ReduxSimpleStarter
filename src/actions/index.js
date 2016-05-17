01f56e849fccb2cd2384fa11de228430d6f02bbb
Marcus R. Brown <mrossbrown@gmail.com>
2016-05-17 05:10:44 -0700

    Complete the map React + Redux weather section.

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '347b6e3d94b58ffb162840a8788fc55c';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
