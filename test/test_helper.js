5a6e1d34f9f0b100f8c4ce14527a6c4001ead04b
Marcus R. Brown <me@igetgam.es>
2016-05-28 23:39:37 -0700

    Submit changes through the end of Section 3.

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,8 +1,8 @@
-import _$ from 'jquery';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
 import jsdom from 'jsdom';
+import jquery from 'jquery';
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import ReactDOM from 'react-dom';
 import chai, { expect } from 'chai';
 import chaiJquery from 'chai-jquery';
 import { Provider } from 'react-redux';
@@ -11,13 +11,10 @@ import reducers from '../src/reducers';
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
-global.navigator = global.window.navigator;
-const $ = _$(window);
-
-chaiJquery(chai, chai.util, $);
+const $ = jquery(global.window);
 
 function renderComponent(ComponentClass, props = {}, state = {}) {
-  const componentInstance =  TestUtils.renderIntoDocument(
+  const componentInstance = TestUtils.renderIntoDocument(
     <Provider store={createStore(reducers, state)}>
       <ComponentClass {...props} />
     </Provider>
@@ -30,7 +27,10 @@ $.fn.simulate = function(eventName, value) {
   if (value) {
     this.val(value);
   }
-  TestUtils.Simulate[eventName](this[0]);
-};
 
-export {renderComponent, expect};
+  TestUtils.Simulate[eventName](this[0]);
+}
+
+chaiJquery(chai, chai.util, $);
+
+export { renderComponent, expect };
