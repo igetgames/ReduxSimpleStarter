614896f0afa68baaa1fd52004f2896de626be63a
Marcus R. Brown <me@igetgam.es>
2016-07-18 10:24:15 -0700

    Scaffold basic app and wire in redux-form.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import App from './components/app';
+import Signin from './components/auth/signin';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = compose(
+  applyMiddleware(),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <Route path="signin" component={Signin} />
+      </Route>
+    </Router>
   </Provider>
   , document.querySelector('.container'));
