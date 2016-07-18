793758b40cb3a4b727377e95ca9cbaed126b4580
Marcus R. Brown <me@igetgam.es>
2016-07-17 20:12:52 -0700

    Add nodemon and initial route.

--- auth/server/index.js
+++ auth/server/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const routes = require('./routes');
 
 const app = express();
 
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
+routes(app);
 
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
