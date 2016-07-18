c50320c9632d67cd526a2093f0fdea0fd3b6fef6
Marcus R. Brown <me@igetgam.es>
2016-07-18 11:46:02 -0700

    Add CORS support to the server.

--- auth/server/index.js
+++ auth/server/index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 mongoose.connect('mongodb://localhost:auth/auth');
@@ -10,6 +11,7 @@ mongoose.connect('mongodb://localhost:auth/auth');
 const app = express();
 
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 routes(app);
 
--- auth/server/routes.js
+++ auth/server/routes.js
@@ -1,15 +1,15 @@
 const Authentication = require('./controllers/authentication');
-const passportService = require('./services/passport');
+require('./services/passport');
 const passport = require('passport');
 
 const requireAuth = passport.authenticate('jwt', { session: false });
 const requireLogin = passport.authenticate('local', { session: false });
 
 module.exports = function (app) {
-  app.get('/', requireAuth, function (req, res, next) {
-    res.send(['fun', 'in', 'the', 'sun']);
+  app.get('/', requireAuth, (req, res) => {
+    res.send({ message: 'Here\'s your hidden content!' });
   });
 
   app.post('/login', requireLogin, Authentication.login);
   app.post('/signup', Authentication.signup);
-}
+};
