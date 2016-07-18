54e794c852be43c0dac6cef5f70c120f067e2691
Marcus R. Brown <me@igetgam.es>
2016-07-17 18:48:58 -0700

    Complete Middleware section up to the Middleware Stack lecture.

---
+++ src/components/user_list.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+import {fetchUsers} from '../actions';
+
+class UserList extends Component {
+  componentWillMount() {
+    this.props.fetchUsers();
+  }
+
+  renderUser(user) {
+    return (
+      <div key={user.id} className="card card-block">
+        <h4 className="card-title">{user.name}</h4>
+        <p className="card-text">{user.company.name}</p>
+        <a className="btn btn-primary" href={'mailto:' + user.email}>Email</a>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="user-list">
+        {this.props.users.map(this.renderUser)}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { users: state.users };
+}
+
+export default connect(mapStateToProps, { fetchUsers })(UserList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
+import users from './users';
+
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  users
 });
 
 export default rootReducer;
