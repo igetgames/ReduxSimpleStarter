81f400da541bb4e89348d33e47ed6915c4647ff7
Marcus R. Brown <me@igetgam.es>
2016-07-18 02:05:17 -0700

    Encrypt passwords on user creation.

--- auth/server/models/user.js
+++ auth/server/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -6,6 +7,24 @@ const userSchema = new Schema({
   password: String
 });
 
+userSchema.pre('save', function(next) {
+  const user = this;
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+
+      user.password = hash;
+      next();
+    });
+  });
+});
 
 const ModelClass = mongoose.model('user', userSchema);
 
