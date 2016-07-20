4dc3f3451df0e24ace074dcb4a5e070c45613a6c
Marcus R. Brown <me@igetgam.es>
2016-07-20 02:31:59 -0700

    Use the server message in the Feature component.

--- src/components/feature.js
+++ src/components/feature.js
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-export default function () {
-  return <div>Welcome to our features!</div>;
+class Feature extends Component {
+  static propTypes = {
+    fetchMessage: PropTypes.func.isRequired,
+    message: PropTypes.string.isRequired
+  }
+
+  componentWillMount() {
+    this.props.fetchMessage();
+  }
+
+  render() {
+    return <div>{this.props.message}</div>;
+  }
 }
+
+function mapStateToProps(state) {
+  return {
+    message: state.feature.message
+  };
+}
+
+export default connect(mapStateToProps, actions)(Feature);
