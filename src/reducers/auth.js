2d5977b404daf71da95e1cb87dc5f38e94dbb001
Marcus R. Brown <me@igetgam.es>
2016-07-19 18:34:00 -0700

    Clear the error state when the authentication state changes.

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -3,10 +3,10 @@ import * as actionTypes from '../actions/types';
 export default function auth(state = {}, action) {
   switch (action.type) {
     case actionTypes.AUTH_USER:
-      return { ...state, authenticated: true };
+      return { ...state, error: '', authenticated: true };
 
     case actionTypes.UNAUTH_USER:
-      return { ...state, authenticated: false };
+      return { ...state, error: '', authenticated: false };
 
     case actionTypes.SET_AUTH_ERROR:
       return { ...state, error: action.payload };
