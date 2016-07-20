9233f77c1b30f31a187a2d397d39282c5905d1c3
Marcus R. Brown <me@igetgam.es>
2016-07-19 18:59:47 -0700

    Authenticate the user if there is a stored JWT.

--- src/index.js
+++ src/index.js
@@ -13,14 +13,21 @@ import Feature from './components/feature';
 import Welcome from './components/welcome';
 import requireAuth from './components/auth/requireAuth';
 import reducers from './reducers';
+import { AUTH_USER } from './actions/types';
 
 const createStoreWithMiddleware = compose(
   applyMiddleware(thunk),
   window.devToolsExtension ? window.devToolsExtension() : f => f
 )(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+if (token) {
+  store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
         <IndexRoute component={Welcome} />
